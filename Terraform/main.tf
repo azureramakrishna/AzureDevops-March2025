420348b031d0ca89b6ed5fce592ed546fb942a48
Ramakrishna <saanvikitsetp2024@outlook.com>
2025-04-02 08:36:25 +0530

    Azure VM with terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,14 @@
+
+# azure terraform provider version
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.0.0"
+    }
+  }
+}
+
 # Azure terraform provider configuration
 provider "azurerm" {
   features {}
@@ -6,18 +17,147 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
   tags     = var.tags
 }
 
 # Create a storage account
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "sa" {
   name                     = lower(var.storage_account_name)
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags                     = var.tags
+}
+
+# Create a Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.virtual_network_address
+
+  tags = var.tags
+}
+
+# Create a Subnet
+resource "azurerm_subnet" "snet" {
+  depends_on           = [azurerm_virtual_network.vnet]
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address
+}
+
+# Create a Public IP
+resource "azurerm_public_ip" "pip" {
+  name                = var.public_ip_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+
+  tags = var.tags
+}
+
+# Create a Network Interface
+resource "azurerm_network_interface" "nic" {
+  depends_on          = [azurerm_public_ip.pip, azurerm_subnet.snet]
+  name                = var.nic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.snet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+# Create a Netowrk security group
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = var.tags
+}
+
+# Associate the Network Security Group with the subnet
+resource "azurerm_subnet_network_security_group_association" "snet_nsg" {
+  depends_on                = [azurerm_network_security_group.nsg, azurerm_subnet.snet]
+  subnet_id                 = azurerm_subnet.snet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Create a Virtual Machine
+resource "azurerm_windows_virtual_machine" "vm" {
+  depends_on          = [azurerm_network_interface.nic]
+  name                = var.virtual_machine_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.virtual_machine_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    name                 = "${var.virtual_machine_name}-osdisk-01" //saanvikit-vm-osdisk-01
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+# Create a Managed Disk
+resource "azurerm_managed_disk" "data_disk" {
+  name                 = "${var.virtual_machine_name}-datadisk-01" //saanvikit-vm-datadisk-01
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
+
+# Attach the Managed Disk to the Virtual Machine
+resource "azurerm_virtual_machine_data_disk_attachment" "vm_datadisk" {
+  managed_disk_id    = azurerm_managed_disk.data_disk.id
+  virtual_machine_id = azurerm_windows_virtual_machine.vm.id
+  lun                = "10"
+  caching            = "ReadWrite"
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -29,5 +29,59 @@ variable "subscription_id" {
   description = "The Azure subscription ID."
   type        = string
   default     = "2e28c82c-17d7-4303-b27a-4141b3d4088f"
-  
+}
+
+variable "virtual_network_name" {
+  type    = string
+  default = "saanvikit-vnet"
+}
+
+variable "virtual_network_address" {
+  type    = list(string)
+  default = ["10.0.0.0/24"]
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "saanvikit-subnet"
+}
+
+variable "subnet_address" {
+  type    = list(string)
+  default = ["10.0.0.0/24"]
+}
+
+variable "public_ip_name" {
+  type    = string
+  default = "saanvikit-public-ip"
+}
+
+variable "nic_name" {
+  type    = string
+  default = "saanvikit-nic"
+}
+
+variable "nsg_name" {
+  type    = string
+  default = "saanvikit-nsg"
+}
+
+variable "virtual_machine_name" {
+  type    = string
+  default = "saanvikit-vm"
+}
+
+variable "virtual_machine_size" {
+  type    = string
+  default = "standard_d2s_v3"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
 }
