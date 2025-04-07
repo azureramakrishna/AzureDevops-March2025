e8e9d6b6c5ecd2f5a12a8e062df0e195dd32018f
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2025-04-07 08:16:12 +0530

    fix

--- Terraform/Count/main.tf
+++ Terraform/Count/main.tf
@@ -1,23 +1,3 @@
-# Azure terraform provider configuration
-provider "azurerm" {
-  features {}
-
-  subscription_id = var.subscription_id
-  client_id       = "d46e6eca-91fd-4b00-8467-61efcb383cc8"
-  client_secret   = null
-  tenant_id       = "459865f1-a8aa-450a-baec-8b47a9e5c904"
-}
-
-# Terraform Backend
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "storage-resource-group"
-    storage_account_name = "saanvikit"            # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"              # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "sa.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
-}
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
